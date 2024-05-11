9e003164e2cc1fbc373e6cd5e9ba841213ae0536
Anthony Cursewl <135174002+JoeCursewl@users.noreply.github.com>
2024-05-11 12:02:51 -0400

    Fixed bug Packages /

--- src/controllers/info.controller.js
+++ src/controllers/info.controller.js
@@ -5,11 +5,13 @@ export const getPackages = async (req, res) => {
     const token = req.headers.authorization;
     const { page } = req.params
 
+    console.log(page)
     if (!token) {
         return res.status(401).json({ message: "No se proporcionó un token" });
     }
 
     try {
+
         const decoded = jwt.verify(token, SECRET_KEY);
         const tQuery = "SELECT * FROM sessiontokens WHERE _id_user = $1 and stoken = $2";
         
@@ -18,7 +20,7 @@ export const getPackages = async (req, res) => {
             return res.status(401).json({ message: "INVALID TOKEN REJECTED" });
         }
 
-        const gQuery = `SELECT * FROM admin_packages ORDER BY created_at ASC LIMIT 10 OFFSET (${page} - 1) * 10`;
+        const gQuery = `SELECT * FROM admin_packages ORDER BY created_at ASC LIMIT 10 OFFSET ${(page - 1) * 10}`;
         const response = await pool.query(gQuery);
 
         return res.status(200).json({ message: response.rows });
