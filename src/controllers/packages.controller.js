21be246b646a3512ba8c36f6aa5f0d7d86c89b43
Anthony Cursewl <135174002+JoeCursewl@users.noreply.github.com>
2024-05-17 10:17:49 -0400

    Added updatePackages (Fixed) /

--- src/controllers/packages.controller.js
+++ src/controllers/packages.controller.js
@@ -1,95 +1,107 @@
 import jwt from "jsonwebtoken";
 const { SECRET_KEY } = process.env;
-import { error_messgae_400, error_messgae_401, error_messgae_500 } from "../config.brd.js";
+import {
+  error_messgae_400,
+  error_messgae_401,
+  error_messgae_500,
+} from "../config.brd.js";
 import pool from "../db.brd.js";
 import e from "express";
 
 export const getPackage = async (req, res) => {
-    const token = req.headers.authorization;
-    if (!token) {
-        return res.status(401).json({ message: "No se proporcionó un token" });
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: "No se proporcionó un token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const tQuery =
+      "SELECT * FROM sessiontokens WHERE _id_user = $1 and stoken = $2";
+    const result = await pool.query(tQuery, [decoded._id, token]);
+
+    if (result.rowCount === 0) {
+      return res.status(401).json({ message: error_messgae_401 });
+    }
+    const { id } = req.params;
+    const gQuery = "SELECT * FROM admin_packages WHERE _id_package = $1";
+    const response = await pool.query(gQuery, [id]);
+
+    if (response.rowCount === 0) {
+      return res.status(404).json({ message: "No se encontró el paquete" });
     }
 
-    try {
-        const decoded = jwt.verify(token, SECRET_KEY);
-        const tQuery = "SELECT * FROM sessiontokens WHERE _id_user = $1 and stoken = $2";
-        const result = await pool.query(tQuery, [decoded._id, token]);
-
-        if (result.rowCount === 0) {
-            return res.status(401).json({ message: error_messgae_401 })
-        }
-        const { id } = req.params;
-        const gQuery = 'SELECT * FROM admin_packages WHERE _id_package = $1';
-        const response = await pool.query(gQuery, [id]);
-
-        if (response.rowCount === 0) {
-            return res.status(404).json({ message: 'No se encontró el paquete' });
-        }
-
-        return res.status(200).json({ message: response.rows });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: error_messgae_500 });
-    }
-}
-
+    return res.status(200).json({ message: response.rows });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: error_messgae_500 });
+  }
+};
 
 export const updatePackage = async (req, res) => {
-    const token = req.headers.authorization;
-    if (!token) {
-        return res.status(401).json({ message: "No se proporcionó un token" });
-    } 
-    try {
-        const decoded = jwt.verify(token, SECRET_KEY);
-        const tQuery = "SELECT * FROM sessiontokens WHERE _id_user = $1 and stoken = $2";
-        const result = await pool.query(tQuery, [decoded._id, token]);
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: "No se proporcionó un token" });
+  }
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const tQuery =
+      "SELECT * FROM sessiontokens WHERE _id_user = $1 and stoken = $2";
+    const result = await pool.query(tQuery, [decoded._id, token]);
 
-        if (result.rowCount === 0) {
-            res.status(401).json({ message: error_messgae_401 });
-        }
-
-        const { id } = req.params;
-        const { name, description, price } = req.body;
-        const uQuery = 'UPDATE admin_packages SET name = $1, description = $2, price = $3 WHERE uuid = $4';
-        const response = await pool.query(uQuery, [name, description, price, id]);
-
-        if (response.rowCount === 0) {
-            return res.status(400).json({ message: error_messgae_400 });
-        }
-
-        return res.status(200).json({ message: 'Paquete actualizado' });
-    } catch (error) {
-        console.log(error.message);
-        return res.status(500).json({ message: error_messgae_500 });
+    if (result.rowCount === 0) {
+      res.status(401).json({ message: error_messgae_401 });
     }
-}
+
+    const { id } = req.params;
+    const { name_package, description_package, price_package } = req.body;
+    const uQuery =
+      "UPDATE admin_packages SET name_package = $1, description_package = $2, price_package = $3 WHERE _id_package = $4";
+    const response = await pool.query(uQuery, [
+      name_package,
+      description_package,
+      price_package,
+      id,
+    ]);
+
+    if (response.rowCount === 0) {
+      return res.status(400).json({ message: error_messgae_400 });
+    }
+
+    return res.status(200).json({ message: "Paquete actualizado" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: error_messgae_500 });
+  }
+};
 
 export const deletePackage = async (req, res) => {
-    const token = req.headers.authorization;
-    if (!token) {
-        return res.status(401).json({ message: "No se proporcionó un token" });
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: "No se proporcionó un token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const tQuery =
+      "SELECT * FROM sessiontokens WHERE _id_user = $1 and stoken = $2";
+    const result = await pool.query(tQuery, [decoded._id, token]);
+
+    if (result.rowCount === 0) {
+      return res.status(401).json({ message: error_messgae_401 });
     }
 
-    try {
-        const decoded = jwt.verify(token, SECRET_KEY);
-        const tQuery = "SELECT * FROM sessiontokens WHERE _id_user = $1 and stoken = $2";
-        const result = await pool.query(tQuery, [decoded._id, token]);
+    const { id } = req.params;
+    const dQuery = "DELETE FROM admin_packages WHERE uuid = $1";
+    const response = await pool.query(dQuery, [id]);
 
-        if (result.rowCount === 0) {
-            return res.status(401).json({ message: error_messgae_401 });
-        }
-
-        const { id } = req.params;
-        const dQuery = 'DELETE FROM admin_packages WHERE uuid = $1';
-        const response = await pool.query(dQuery, [id]);
-
-        if (response.rowCount === 0) {
-            return res.status(404).json({ message: 'Paquete no encontrado' });
-        }
-
-        return res.status(200).json({ message: 'Paquete eliminado' });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: error_messgae_500 });
+    if (response.rowCount === 0) {
+      return res.status(404).json({ message: "Paquete no encontrado" });
     }
-}
+
+    return res.status(200).json({ message: "Paquete eliminado" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: error_messgae_500 });
+  }
+};
