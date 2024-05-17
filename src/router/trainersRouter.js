020a10cf5ba7dad66c8dfc8c4a65059072c1bd6c
Anthony Cursewl <135174002+JoeCursewl@users.noreply.github.com>
2024-05-17 12:59:20 -0400

    Added Trainers UpdateTrainer /

--- src/router/trainersRouter.js
+++ src/router/trainersRouter.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 const trainersRouter = Router()
-import { getTrainer } from '../controllers/trainers.controller.js';
+import { getTrainer, updateTrainer } from '../controllers/trainers.controller.js';
 
 trainersRouter.get('/get/:id', getTrainer)
+
+trainersRouter.patch('/update/:id', updateTrainer)
 
 export default trainersRouter
 
